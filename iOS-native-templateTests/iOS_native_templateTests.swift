da3a2bb48065af57ab3222aff534aa06a10c27a2
shailandra-suriyal <shailandra.suriyal@hp.com>
2018-01-03 12:11:55 -0700

    Initial Commit

---
+++ iOS-native-templateTests/iOS_native_templateTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_native_templateTests.swift
+//  iOS-native-templateTests
+//
+//  Created by Shailendra Suriyal on 1/3/18.
+//  Copyright © 2018 intimetec. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_native_template
+
+class iOS_native_templateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
